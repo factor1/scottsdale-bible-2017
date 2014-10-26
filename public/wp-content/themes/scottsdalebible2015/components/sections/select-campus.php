a8b9779af915b79d8363891b785ddb6900eb0d15
James R. Latham <jamesl@splitconcepts.com>
2014-10-25 21:53:26 -0700

    Added background image, setup all HTML and styles for select campus template part

--- public/wp-content/themes/scottsdalebible2015/components/sections/select-campus.php
+++ public/wp-content/themes/scottsdalebible2015/components/sections/select-campus.php
@@ -1,1 +1,29 @@
 <?php if(!isset($wp)) { return; } ?>
+
+<section class="select-campus">
+    <div class="row">
+        <ul class="no-bullet">
+            <li>
+                <h3>Select <span>your</span> campus <i class="fa fa-chevron-down"></i></h3>
+            </li>
+            <li>
+                <a href="#"><span>Shea</span> Campus</a>
+            </li>
+            <li>
+                <a href="#"><span>Cactus</span> Campus</a>
+            </li>
+            <li>
+                <a href="#"><span>Worship</span> Center</a>
+            </li>
+            <li>
+                <a href="#"><span>Venue</span> Community</a>
+            </li>
+            <li>
+                <a href="#"><span>Grace</span> Chapel</a>
+            </li>
+            <li>
+                <a href="#"><span>Hayden</span> Chapel</a>
+            </li>
+        </ul>
+    </div>
+</section>
