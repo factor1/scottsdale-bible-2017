9a25ff387ab9400260ba56cb0387dd9996d99fac
Matthew Adams <mattadams@factor1studios.com>
2014-11-24 14:21:10 -0700

    one more test for content

--- public/wp-content/themes/scottsdalebible2015/components/posts/article.php
+++ public/wp-content/themes/scottsdalebible2015/components/posts/article.php
@@ -17,7 +17,5 @@
     </div>
     <div>
         <?php echo ($c=sb_get_content_field("page_content")) ? $c : get_the_content(); ?>
-        <!-- the content -->
-        <?php the_content();?>
     </div>
 </section>
