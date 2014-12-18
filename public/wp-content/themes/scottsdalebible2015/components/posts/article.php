90b0548e9e5584bcbd691050bd282f1f19d10512
James R. Latham <jamesl@splitconcepts.com>
2014-12-18 14:28:57 -0800

    Added social link triggers to pages/posts, more style/mobile fixes

--- public/wp-content/themes/scottsdalebible2015/components/posts/article.php
+++ public/wp-content/themes/scottsdalebible2015/components/posts/article.php
@@ -8,13 +8,13 @@
         <?php if(!get_field("hide_social_links")) { ?>
         <ul class="inline-list">
             <li>
-                <a href="#"><i class="fa fa-facebook-square"></i>Share this post on Facebook</a>
+                <a href="#" data-trigger-click="st_facebook_large"><i class="fa fa-facebook-square"></i>Share this post on Facebook</a>
             </li>
             <li>
-                <a href="#"><i class="fa fa-twitter-square"></i>Tweet about this post</a>
+                <a href="#" data-trigger-click="st_twitter_large"><i class="fa fa-twitter-square"></i>Tweet about this post</a>
             </li>
             <li>
-                <a href="#"><i class="fa fa-pinterest-square"></i>Pin this post</a>
+                <a href="#" data-trigger-click="st_pinterest_large"><i class="fa fa-pinterest-square"></i>Pin this post</a>
             </li>
         </ul>
         <?php } ?>
