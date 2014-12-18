062f195c0c72e9cf1627b290ffc7948aae79e037
James R. Latham <jamesl@splitconcepts.com>
2014-12-18 13:37:39 -0800

    Made social links hidden on search results

--- public/wp-content/themes/scottsdalebible2015/components/posts/search-list.php
+++ public/wp-content/themes/scottsdalebible2015/components/posts/search-list.php
@@ -1,7 +1,7 @@
 <?php get_template_part("components/posts/nav"); ?>
 <?php while(have_posts()) { the_post(); ?>
     <div class="row">
-        <div class="small-12">
+        <div class="small-12 columns">
             <h3><a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a></h2>
             <?php the_excerpt(); ?>
         </div>
