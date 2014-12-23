26e03193334be385fbd48c4e168607dc9643d9e0
Matthew Adams <mattadams@factor1studios.com>
2014-12-22 23:25:13 -0700

    New footer tracking

---
+++ public/wp-content/themes/scottsdalebible2015/components/sections/footer_tracking.php
@@ -0,0 +1,81 @@
+<!-- Start Quantcast tag -->
+<script type="text/javascript">
+_qoptions={
+qacct:"p-aalJ8sFCvGENQ"
+};
+</script>
+<?php
+if($_SERVER["HTTPS"])
+{
+	echo '<script type="text/javascript" src="https://edge.quantserve.com/quant.js"></script>';
+}
+else
+{
+	echo '<script type="text/javascript" src="http://edge.quantserve.com/quant.js"></script>';
+}
+ ?>
+<noscript>
+<?php
+if($_SERVER["HTTPS"])
+{
+	echo '<img src="https://pixel.quantserve.com/pixel/p-aalJ8sFCvGENQ.gif" style="display: none;" border="0" height="1" width="1" alt="Quantcast"/>';
+}
+else
+{
+	echo '<img src="http://pixel.quantserve.com/pixel/p-aalJ8sFCvGENQ.gif" style="display: none;" border="0" height="1" width="1" alt="Quantcast"/>';
+}
+ ?>
+
+</noscript>
+<!-- End Quantcast tag -->
+<!-- Start Google Analytics -->
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-2243545-1");
+pageTracker._trackPageview();
+} catch(err) {}</script>
+<!-- End Google Analytics -->
+<!-- Start Chartbeat -->
+<script type='text/javascript'>
+    var _sf_async_config={};
+    /** CONFIGURATION START **/
+    _sf_async_config.uid = 2293;
+    _sf_async_config.domain = 'scottsdalebible.com';
+    _sf_async_config.useCanonical = true;
+        /** CONFIGURATION END **/
+    (function(){
+      function loadChartbeat() {
+        window._sf_endpt=(new Date()).getTime();
+        var e = document.createElement('script');
+        e.setAttribute('language', 'javascript');
+        e.setAttribute('type', 'text/javascript');
+        e.setAttribute('src', '//static.chartbeat.com/js/chartbeat.js');
+        document.body.appendChild(e);
+      }
+      var oldonload = window.onload;
+      window.onload = (typeof window.onload != 'function') ?
+         loadChartbeat : function() { oldonload(); loadChartbeat(); };
+    })();
+</script>
+<!-- End Chartbeat -->
+<!-- Start of StatCounter Code for Default Guide -->
+<script type="text/javascript">
+var sc_project=9650106; 
+var sc_invisible=1; 
+var sc_security="c4e0688c"; 
+var scJsHost = (("https:" == document.location.protocol) ?
+"https://secure." : "http://www.");
+document.write("<sc"+"ript type='text/javascript' src='" +
+scJsHost+
+"statcounter.com/counter/counter.js'></"+"script>");
+</script>
+<noscript><div class="statcounter"><a title="web analytics"
+href="http://statcounter.com/" target="_blank"><img
+class="statcounter"
+src="http://c.statcounter.com/9650106/0/c4e0688c/1/"
+alt="web analytics"></a></div></noscript>
+<!-- End of StatCounter Code for Default Guide -->
