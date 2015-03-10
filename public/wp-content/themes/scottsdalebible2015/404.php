b79132b9f5124896a1348f9bfd6cd60f29a1da29
Eric Stout <eric@factor1studios.com>
2015-03-10 11:20:12 -0700

    404 Page Styling

--- public/wp-content/themes/scottsdalebible2015/404.php
+++ public/wp-content/themes/scottsdalebible2015/404.php
@@ -5,7 +5,14 @@ get_header();
 ?>
 <section class="not-found">
     <div class="row">
-        <h2>Not Found</h2>
+
+        	<h2>This is somewhat embarrassing, isn't it?</h2>
+	        <h4>It seems we can't find what you are looking for. Perhaps searching can help.</h4>
+	        <div class="medium-6 medium-centered columns" style="margin-top: 50px;">
+		        <form data-action="<?php echo get_option('siteurl'); ?>">
+			         <i class="fa fa-search"></i><input type="text" name="search" value="" placeholder="Search" style="width:80%; display: inline-block; margin: 0 10px; outline: none; box-shadow: none;" />
+		        </form>
+	        </div>
     </div>
 </section>
 <?php
