f46a5a8d1af1e1eef9c83942b368544a039c9c00
Eric Stout <eric@factor1studios.com>
2015-05-01 09:50:12 -0700

    404 page

--- public/wp-content/themes/scottsdalebible2015/404.php
+++ public/wp-content/themes/scottsdalebible2015/404.php
@@ -9,8 +9,8 @@ get_header();
         	<h2>This is somewhat embarrassing, isn't it?</h2>
 	        <h4>It seems we can't find what you are looking for. Perhaps searching can help.</h4>
 	        <div class="medium-6 medium-centered columns" style="margin-top: 50px;">
-		        <form data-action="<?php echo get_option('siteurl'); ?>">
-			         <i class="fa fa-search"></i><input type="text" name="search" value="" placeholder="Search" style="width:80%; display: inline-block; margin: 0 10px; outline: none; box-shadow: none;" />
+		        <form action="<?php bloginfo('siteurl'); ?>" id="searchform" method="get">
+			         <i class="fa fa-search"></i> <input type="search" id="s" name="s" Placeholder="Enter Search Term" style="width:80%; display: inline-block; margin: 0 10px; outline: none; box-shadow: none;">
 		        </form>
 	        </div>
     </div>
