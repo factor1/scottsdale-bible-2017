2164e8b90cb74711f46b5c8c6c3b1fc4ad64afa4
Eric Stout <eric@factor1studios.com>
2015-12-11 10:35:12 -0700

    Adds winter wonder shortcode and css file

---
+++ public/wp-content/themes/scottsdalebible2015/shortcodes/winterwonder.php
@@ -0,0 +1,303 @@
+<link rel="stylesheet" href="<?php bloginfo('template_url');?>/css/winterwonder.css">
+
+<div class="winterwonder">
+	
+<!--<div class="row">
+	<h2>Program</h2>
+	
+
+
+	<h3> Overture </h3>
+	<p><strong> “Miracle of Christmas”/”Carol of the Bells”	</strong>........ Scottsdale Bible Orchestra, Faith Collins, Erin Collins, Hannah Montgomery, Julie Kassenbrock – dance</p>
+
+ 
+</div>
+
+
+<div class="row">
+	<div class="medium-6 columns">
+		<h3> ACT I </h3>
+		<p><strong> “Carol of the Bells”</strong>........ Derek Brandt, Kerri Jones, Carson Parks, Kacey Pearson, Brooklyn Peterson, Troy Peterson</p>
+		<p><strong> White Pants </strong>........ Steve Eriksson & Rustin Rossello </p>
+		<p><strong> “Jingle Bells” </strong>........ Scottsdale Bible Choir, Handbells & Dance Troupe </p>
+		<p><strong> Christmas Around the World </strong></p>
+		<p><strong> Mexico – “Feliz Navidad”	</strong>........ Rita Kurtz & Laura Milus </p>
+		<p><strong> England – Carol Medley </strong>........ Adam Coburn, Georgia Dolph, Ron Fisher, Mary Henry, Julie Michael, Tom Vinopal, Mary Whaley, Mat Yokoyama </p>
+		<p><strong> Russia – “Trepak” </strong>........ Eva Britton, Erin Collins, Kayl Hill, Isabella King, Hannah Lopez, Sofia Lopez, Avery Okland, Reese Okland</p>
+		<p><strong> Sweden – “Sankta Lucia” </strong>........ Rebecca Dias </p>
+		<p><strong> Switzerland </strong></p>
+		<p><strong> Scotland – “Little Drummer Boy” </strong>........ Michael McClanathan – bagpipe, Calem Reimche & Alec Sanchez – percussion</p>
+		<p><strong> Austria – “Stille Nacht” </strong>........ David Britton – solo, Gerald Fercho - guitar </p>
+		<p><strong> Tanzania </strong></p>
+		<p><strong> America – “Jingle Bell Rock” </strong>........ Matt Taylor, Ashlyn Bentley, Faith Collins, Brooke Goodale, Dorian Harris & Scottsdale Bible Children’s Choir</p>
+		<p><strong> Adeste Fideles</strong>........Kristina Bubar, Margaux Cavitt, Gabby Vatistas</p>
+		<p><strong> Compassion Project</strong></p>
+	 
+	</div>
+		
+		
+	<div class="medium-6 columns">
+		<h3> INTERMISSION </h3>
+			
+		<h3> ACT II </h3>
+		<p><strong> “It’s the Most Wonderful Time of the Year” </strong>........ Carson Parks – trombone solo</p>
+		<p><strong> “Hallelujah”</strong>........ Derek Brandt – solo, Debra Murrow – iPad artist </p>
+		<p><strong> Spoken Word </strong>........ Hansley Musotte </p>
+		<p><strong> “O Come, O Come Emmanuel” </strong>........ Lizzy Tuten – Mary, Andy Hone – Joseph </p>
+		<p><strong> “Mary, Did You Know?” </strong>........ Leah Deeney (Thursday, Saturday @ 1 & 4), Katie Rodin (Friday, Saturday @ 7, Sunday @ 2 & 5), Julie Kassenbrock – dance </p>
+		<p><strong> “Sing We Now of Christmas”	</strong>........ Nativity Cast </p>
+		<p><strong> Pastoral Invitation </strong></p>
+		<p><strong> “O Holy Night” </strong>........ Donielle Palomo – solo, Marissa Dudek & Megan Whitley – dance </p>
+		<p><strong> “Hallelujah Chorus” </strong></p>
+	</div>
+</div> -->
+
+
+
+<div class="row">
+	<h2> Winter Wonder Cast and Participants </h2>
+	
+<div class="medium-9 medium-centered columns">
+	
+	<h3> Winter Wonder Opener </h3>
+	<p> Mia Wines <span class="detail">– choreographer</span> </p>
+	
+	<p>Ashlyn Bentley, Erin Collins, Faith Collins, Becca Dominguez, Brooke Goodale, Olivia Helton, Julie Kassenbrock, Eliza Krolick, Maddy Krolick, Claire Kurtz, Haley Marks, Reece Marks, Caileigh McBride, Maggie McBride, Hannah Montgomery, Avery Okland, Emme Paisley, Peyton Peterson, Sophia Smith, Analise Wachtel, Andrew Wachtel, Rhyan Wilson, Taro Yokoyama</p>
+	
+ 
+	
+	<h3> Winter Wonder Orchestra</h3>
+	<p> Joe Bubar <span class="detail">- director,</span> Sandee Coppola <span class="detail">- accompanist</span></br> 
+	 <span class="emphasis">Violin 1 </span> Karen Schreck, Cristina Alasu, Lisa Elliott, Bud Locker, Amy Schroader, Kyle Malkin </br>
+	 <span class="emphasis">Violin 2 </span> Beth Benson-Swain, Ann Sandford, Joan Stempski, Sandy Wallace, Patti Boyce, Christina Reimche</br>
+	 <span class="emphasis">Viola </span> Marg Loewen, Katie Paulsen, Alexis Pillow </br>
+	 <span class="emphasis">Cello </span> Robin Dias, Terri Clark, Emily Hayes </br>
+	 <span class="emphasis">Bass </span> Bob Larson, Danyell Wilt </br>
+	 <span class="emphasis">Trumpet </span> Mark Coats, Paul Alasu, Marty Sellberg, Harry Koehnemann, Braden Reimche </br>
+	 <span class="emphasis">Trombone </span> Bob Anderson, Bim Fritz, Bryce Good, Doug Nixon, Carson Parks </br>
+	 <span class="emphasis">Baritone </span> Bruce Meyer, John Donnell, Amber Jarosz </br>
+	 <span class="emphasis">Tuba </span>  Angel Boici, Danielle Rothleutner </br>
+	 <span class="emphasis">French Horn </span> Stacey D’Abate, Dean Andersen, Robert Dickinson, Benn Smith </br>
+	 <span class="emphasis">Alto Saxophone </span> Alan Knight, Kyle Karasch </br>
+	 <span class="emphasis">Tenor Saxophone </span> David Parrott, Jim Hawthorne </br>
+	 <span class="emphasis">Flute </span> Sheri MacDonald, Leslie Lewis, Tricia Fox </br>
+	 <span class="emphasis">Oboe </span> Shayne Budin </br>
+	 <span class="emphasis">Clarinet </span> Melanie McBride, Dawn Anvik </br>
+	 <span class="emphasis">Bass </span> Clarinet Karen DeMoss</br>
+	 <span class="emphasis">Percussion </span> Stewart Milus, Chris Lindblade, Joey Manuele</br>
+	 <span class="emphasis">Piano </span> Joe Bubar</br>
+	 <span class="emphasis">Keyboards </span> Sandee Coppola</br>
+	 <span class="emphasis">Electric Guitar </span> Ed Heisler</br>
+	 <span class="emphasis">Acoustic Guitar </span> Mark Erwin</br>
+	 <span class="emphasis">Bass Guitar </span> Nelson Eubank</br>
+	 <span class="emphasis">Drums </span> Rex Williamson </p>
+	
+ 
+	
+	<h3> Scottsdale Bible Choir</h3>
+	<p> Troy Peterson <span class="detail">– director,</span> Tammy Reimche <span class="detail">- accompanist</span> </p>
+	
+	<p><span class="emphasis">Soprano</span>  Betty Beran, Mary Berbaum, Jenny Black, Cindy Covington, Marilyn DeSanto, Sandra Dill, Angie Domini, Twyla Dudley, Yvonne Heaton, Ruth Ireland, Pamela Jansick, Cyndi Keen, Donna Kent, Sachi Kurtz, Nina Leo, Joyce Martindale, Linda Mattos, Julie Michael, Deborah Moak, Maxine Newsome, Niko Pagni, Chamberlain Peterson, Nancy Pipher, Lynne Ramey, Karen Rummel, Sandra Saenz, Cheryl Slusser, Linda Smith, Margaret Soberg, Linda Stromberg, Blythe Sweeney, Telly Tan, Mary Jane Waddell, Mary Whaley, Veronica Ye</p>
+	
+	<p><span class="emphasis">Alto</span>  Lois Anderson, Marsha Anderson, Gayle Bielanski, Lynn Brown, Fran Cain, Carol Coats, Laurinda Cumming, Penny Derr, Leslie Doan, Georgia Dolph, Penny Eubank, Suzanne Gallaher, Katie Gant, Elsie Gonser, Bonnie Heng, Annette Johnson, Cyndy Johnston, Laura Kluvo, Bev Lambright, Sue Marion, Wanda Martindale, Sharon Meddles, Laura Milus, Danette Moe, Nancy Nixon, Wendy Peterson, Sue Phipps, Terry Sparks, Betsy Way, Natasha Wiener</p>
+	
+	<p><span class="emphasis">Tenor</span> Tim Allen, Michael Andersen, Vera Beck, Magnus Eriksson, David Hone, Theresa Klassen, Bernie Lambright, Tom Loewen, Al McClain, David Parker, John Pilzner, Tom Vinopal, Doug Wallace, Mat Yokoyama</p>
+	
+	<p><span class="emphasis">Bass</span> Adam Coburn, Ron Fisher, Bill Gillen, Budge Gonser, Denny Halvorson, Steven Houston, Judd Jackson, Frank Klassen, Michael Krolick, Bruce McBride, Lonny McClung, Richard Michael, Sergei Ochkur, Don Pipher, Keith Sandford, Warren Soberg, Paul Stromberg, Bill Waddell, Peter Ye </p>
+	
+ 
+	
+	<h3> Grace Chapel Bell Choir</h3>
+	<p> Sandee Coppola and Katie Gant <span class="detail">– directors</span></p> 
+	<p>Dawn Anvik, Carol Coats, Teri Davies, Karen DeMoss, Katie Gant, Carol Ketcherside, Sachi Kurtz, Jeff MacDonald, Sue Marion, Jacque Surrett, Emily Tuggle, Joyce Wieler</p>
+	
+ 
+	
+	<h3> “Jingle Bells” Dancers </h3>
+	<p>Ann Blackman <span class="detail">– choreographer</span></p> 
+	<p>Marissa Dudek, Dorian Harris, Andy Hone, Eve-Ana Ontiveros, Kamryn Smith, Susan Swanson, Halle Switzer, Dexter Tuten, Emma Tuten, Megan Whitley</p>
+	
+ 
+	
+	<h3> “Sankta Lucia” Girls</h3>
+	<p> Charlie Chamberlain, Elsa Eriksson, Amy Gravel, Kaley Gravel, Ella Palomo, Zöe Shewchuk </p>
+	
+ 
+	
+	<h3> Swedish Candle Carriers</h3>
+	<p> Ashlyn Bentley, Faith Collins, Becca Dominguez, Brooke Goodale, Olivia Helton, Eliza Krolick, Maddy Krolick, Claire Kurtz, Haley Marks, Caileigh McBride, Maggie McBride, Reese Okland, Emme Paisley, Peyton Peterson, Bianca Rahme, Kailey Rawdon, Rhyan Wilson, Chloe Wright</p>
+	
+ 
+	
+	<h3> Winter Wonder Children’s Choir</h3>
+	<p> Sandee Coppola and René Suderman <span class="detail">– directors</span> </p>
+	
+	<p> Anya Abello, Derek Abello, Eden Abello, Pax Bleger, Quincy Bowers, Sarah Brennan, Alex Britton, Erin Collins, Becca Dominguez, Charissa Franklin, Amy Gravel, Kaley Gravel, Bella Herbein, Kayl Hill, Olivia Helton, Olivia Holzhauer, Audrey Kurtz, Bria Lindblade, Hannah Lopez, Sam MacDonald, Stephanie MacDonald, Haley Marks, Reece Marks, Caileigh McBride, Maggie McBride, Reed Moore, Reese Okland, Emme Paisley, Ella Palomo, Peyton Peterson, Bianca Rahme, Kailey Rawdon, Sophia Smith, Robyn Teske, Analise Wachtel, Tessa Wachtel, Katherine Welchert, Rhyan Wilson, Chloe Wright, Josh Zakaras</p>
+	
+ 
+	
+	<h3> Nativity Cast</h3>
+	<p> Barb Napoletano, Sachiko Yokoyama and Lori Morgan <span class="detail">– coordination and staging managers</span> </p>
+	
+	<p> Micheal Allen, Aly Cavness, Faith Collins, Claire Gallagher, Grace Gallagher, Kelsey Gallagher, Reagan Gallagher, Adrianna Gorgone, Mark Hirschbirg, Adam Hone, Ethan Ivie, Paul Johnson, Eric Jones, Evelyn Jones, Joshua Jones,  Caleb Jones, Eliza Krolick, Maddy Krolick, Claire Kurtz, Bogar Lorenzo, Noel Lorenzo, Marc Manuele, Vince Manuele, Lauren Markus, Kelsi McAnally, Dave Morgan, Mattie Napoletano, Reuben Palomo, Adam Ponter, Brielle Rothrock, Becca Tunnicliff, Lizzy Tuten, Dexter Tuten, Andrew Wachtel, Taro Yokoyama </p>
+	
+ 
+	
+	<h3> “O Holy Night” Dancers </h3>
+	<p> Ann Blackman <span class="detail">– choreographer,</span> Debbie Vinopal <span class="detail">– assistant choreographer</span></p> 
+	<p>Marissa Dudek, Dorian Harris, Eve-Ana Ontiveros, Kamryn Smith, Halle Switzer with Faith Collins, Claire Kurtz, and Emma Tuten </p>
+	
+ 
+	
+	<h3> Hair and Makeup </h3>
+	<p> Betsy McBride </p>
+	
+ 
+	
+	<h3> Production Crew</h3>
+	<p> Braxton Apana, Josh Best, Derek Brandt, John Buttel, Todd Cook, Valerie Egnash, Amy Farrugia, Jadon Floyd, Karl Forster, Cade Franklin, James George, DJ Hepburn, Sam Lincoln, Nate Lindgren, Caleb Niño, Dave Pennington, Leah Powell, Kevin Stayte, Greg Vatt, John Wallace, Mark Whitehouse, Amber Wong</p>
+	
+ 
+	
+	<h3>Stage Crew</h3>
+	<p> Barb Napoletano and Loyd Sparks <span class="detail">– stage managers</span> </p>
+	
+	<p> Alex Apodaca, Paul Johnson, Hadden Newlander, Tim Reimche</p>
+	
+ 
+	
+	<h3> Sets/Props/Costumes </h3>
+	<p> Leslie Doan, Pam Jansick, Jan Nickel, Nancy Reinauer, Loyd Sparks, Jacque Surrett </p>
+	
+	<div class="centered">
+		<h3></h3>
+		<p class="vip"><span class="emphasis"> Executive Producer</span> Troy Peterson </p>
+		<p class="vip"><span class="emphasis">Co-producers</span> Julia Hamilton, Tammy Reimche </p>
+		<p class="vip"><span class="emphasis">Production Director</span>  Nick Palomo </p>
+		<p class="vip"><span class="emphasis">Instrumental Director and Arranger</span> Joe Bubar </p>
+		<p class="vip"><span class="emphasis">Audio Engineer</span> Kevin Anvik </p>
+		<p class="vip"><span class="emphasis">Video and Lighting Design</span> Josh Rothrock </p>
+		<p class="vip"><span class="emphasis">Video Editor</span> Jesse Lopez </p>
+		<p><span class="emphasis">Graphic Design </span> Mike Oliveros </p>
+	</div>
+</div>
+</div>
+ 
+ 
+ 
+<div class="row">
+	<h2>Production Crew</h2>
+	
+	<div class="row">
+		<div class="medium-9 medium-centered columns">
+			
+			<ul class="small-block-grid-1 medium-block-grid-4">
+				<?php
+				global $post;
+				$args = array( 
+						'numberposts' 				=> -1, 
+						'post_type'              	=> 'f1_staffgrid_cpt',	
+						'f1_staffgrid_tax' 			=> 'production-crew', // Department Taxonomy (per site).
+						'meta_key' 					=> 'last_name',
+						'orderby'					=> 'meta_value', 
+						//'post__in' 				=> array( 2, 5, 12, 14, 20 );,  // Use ID's in an array for specific ordering.
+						'order' 						=> 'ASC'
+						);
+	
+				$staffgrid = get_posts( $args );
+				foreach( $staffgrid as $post ) :	setup_postdata($post); ?>
+				
+				<li>
+					<a data-remodal-target="modal-<?php the_ID(); ?>" style="text-align:center">
+						<!-- Photo -->
+						<div class="f1_mentor_photo_container">
+							<?php if(has_post_thumbnail()) {
+								the_post_thumbnail();
+								} else {	}
+							?>
+						</div>
+						
+						<div class="f1_mentor_summary_name"><strong><?php the_field( "first_name" ); ?>&nbsp;<?php the_field( "last_name" ); ?></strong></div>
+						<div class="f1_mentor_summary_title"><small><?php if(get_field( "title" )) : the_field( "title" ); endif; ?></small></div>
+					</a>
+				</li>
+				
+				<!-- Remodal -->
+				<div class="remodal" data-remodal-id="modal-<?php the_ID();?>">
+					<div class="row">
+						<div class="medium-4 columns">
+							<div class="f1_mentor_details_photo_container">
+								<?php if(has_post_thumbnail()) {
+									the_post_thumbnail();
+								} else {	}
+								?>
+							</div>
+
+							<div class="f1_mentor_details_social_container">
+								<?php if(get_field( "phone" )) : echo('Phone: '); the_field( "phone" ); endif; ?></br>
+								<?php if(get_field( "email_address" )) : echo('<a href="mailto:'); the_field( "email_address"); echo('">Email '); the_field( "first_name" ); echo('</a></br>'); endif; ?>
+
+								<?php if(get_field( "twitter_url" )) : echo('<a href="'); the_field( "twitter_url" ); echo('">Twitter</a>');  endif; ?>
+								<?php if(get_field( "facebook_url" )) : echo('<a href="'); the_field( "facebook_url" ); echo('">Facebook</a>');  endif; ?>
+								<?php if(get_field( "linkedin_irl" )) : echo('<a href="'); the_field( "linkedin_url" ); echo('">LinkedIn</a>');  endif; ?>
+								<?php if(get_field( "instagram_url" )) : echo('<a href="'); the_field( "instagram_url" ); echo('">Instagram</a>');  endif; ?>
+							</div>
+						</div>
+
+						<div class="medium-8 columns">
+							<p class="f1_mentor_details_name"><strong><?php the_field( "first_name" ); ?>&nbsp;<?php the_field( "last_name" ); ?></strong></br>
+							<span class="f1_mentor_summary_title"><small>	<?php if(get_field( "title" )) : the_field( "title" ); endif; ?></small></span></p>
+
+							<div class="f1_mentor_details_bio">
+								<?php if(get_field( "staff_bio" )) : the_field( "staff_bio" ); endif; ?>
+							</div>
+						</div>
+					</div>
+				</div>
+				<?php endforeach; wp_reset_postdata();?>
+			</ul>
+			
+		</div>
+	</div>
+
+</div>
+ 
+ 
+
+<!--<div class="row">
+	<div class="medium-6 columns">
+		<h3> Choir </h3>
+		<p> Tim Allen, Lois Anderson, Marsha Anderson, Michael Anderson, Vera Beck, Betty Beran, Mary Berbaum, Gayle Bielanski, Jenny Black, Lynn Brown, Fran Cain, Carol Coats, Adam Coburn, Cindy Covington, Laurinda Cumming, Penny Derr, Marilyn DeSanto, Sandra Dill, Leslie Doan, Georgia Dolph, Angie Domini, Twyla Dudley, Magnus Eriksson, Penny Eubank, Ron Fisher, Suzanne Gallaher, Katie Gant, Bill Gillen, Budge Gonser, Elsie Gonser, Denny Halvorson, Yvonne Heaton, Bonnie Heng, David Hone, Steven Houston, Ruth Ireland, Judd Jackson, Pam Jansick, Annette Johnson, Cyndy Johnston, Cyndi Keen, Donna Kent, Frank Klassen, Theresa Klassen, Laura Kluvo, Michael Krolick, Sachi Kurtz, Bernie Lambright, Bev Lambright, Nina Leo, Tom Loewen, Sue Marion, Joyce Martindale, Wanda Martindale, Linda Mattos, Bruce McBride, Al McClain, Lonny McClung, Sharon Meddles, Julie Michael, Richard Michael, Laura Milus, Deborah Moak, Danette Moe, Maxine Newsome, Nancy Nixon, Sergei Ochkur, Niko Pagni, David Parker, Chamberlain Peterson, Wendy Peterson, Sue Phipps, John Pilzner, Don Pipher, Nancy Pipher, Lynne Ramey, Karen Rummel, Sandra Saenz, Keith Sandford, Cheryl Slusser, Linda Smith, Margaret Soberg, Warren Soberg, Terry Sparks, Linda Stromberg, Paul Stromber, Blythe Sweeney, Telly Tan, Tom Vinopal, Bill Waddell, Mary Jane Waddell, Doug Wallace, Betsy Way, Mary Whaley, Natasha Wiener, Peter Ye, Veronica Ye, Mat Yokoyama </p>
+	</div>
+
+
+	<div class="medium-6 columns">	
+		<h3> Orchestra </h3>
+		<p> Cristina Alasu, Paul Alasu, Bob Anderson, Dawn Anvik, Dean Anderson, Beth Benson-Swain, Angel Boici, Patti Boyce, Terri Clarke, Mark Coats, Sandee Coppola, Stacey D’Abate, Karen DeMoss, Robin Dias, Robert Dickinson, John Donnell, Lisa Elliott, Mark Erwin, Nelson Eubank, Sharon Falada, Bim Fritz, Bryce Good, Emily Hayes, Ed Heisler, Amber Jarosz, Erik Ketcherside, Alan Knight, Bob Larson, Chris Lindblade, Bud Locker, Marg Loewen, Sheri MacDonald, Joey Manuele, Melanie McBride, Bruce Meyer, Stewart Milus, Doug Nixon, David Parrott, Katie Paulsen, Alexis Pillow, Danielle Rothleutner, Ann Sandford, Karen Schreck, Amy Schroader, Benn Smith, Joan Stempski, Sandy Wallace, Rex Williamson  </p>
+	</div>
+	 
+</div> -->
+
+<div class="row">
+	<h2> Christmas Giving Project </h2>
+	
+	<div class="medium-9 medium-centered columns">
+		<h3></h3>
+		
+		<p>Each year at Winter Wonder, we choose an opportunity to meet a specific need in our community and extend generosity in Christ’s name. This year, we’re adopting four Title 1 schools in Scottsdale (Hohokam, Navajo, Tavan & Yavapai) and partnering with Healthy Packs and Concerned Citizens for Community Health to help children experiencing weekend hunger. The Healthy Packs program provides a $4-6 pack of nutritious food to sustain a child from Friday evening through Sunday. We plan to fully fund the program at these four schools for the ’15/’16 school year.</p>
+		
+		<p><a href="http://ccch.azurewebsites.net/programs/healthy-packs" target="_blank">Learn more about the Healthy Packs Program here.</a> This <a href="http://www.azcentral.com/story/news/local/scottsdale/2014/09/25/scottsdale-schools-healthy-packs-feed-kids/16208929/?from=global&sessionKey=&autologin=" target="_blank">AZ Central article</a> shares more about the impact of this program on Scottsdale kids.</p>
+		
+		<p>In addition to funding the Healthy Packs program, we will also provide Christmas stockings for approximately 1,500 children and gifts for teachers and staff at three of these schools. Gifts will be presented at surprise assemblies on December 17, 2015. Our vision is to share Christ’s love and build lasting relationships at each of these schools.</p>
+		
+		<p>If you would like to participate in supporting this project, <a href="https://scottsdalebible.onlinegiving.cc/donate/login" target="_blank">you may give online here.</a> You may also give by texting “give” to 480.771.1722 or via check to Scottsdale Bible Church.  Thank you for your generosity toward this effort!</p>
+
+	</div>
+</div>
+
+
+<div class="row centered">
+	<p class="italictext winterfoot"> Are you interested in being part of Winter Wonder 2016? Email <a href="mailto:worship@scottsdalebible.com">worship@scottsdalebible.com</a> and indicate your area of experience/interest. </p>
+</div>
+
+</div>
