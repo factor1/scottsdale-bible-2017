7677db131829dab2909e9b42dddbeb549fb14fe7
mattada <mattadams@factor1studios.com>
2016-05-23 14:07:04 -0700

    New jobs page
    Signed-off-by: mattada <mattadams@factor1studios.com>

---
+++ public/wp-content/themes/scottsdalebible2015/page-jobs.php
@@ -0,0 +1,8 @@
+<?php
+/*Template Name: Jobs */
+get_header(); ?>
+
+<iframe style="height:100vh; width:100vw;" src="http://jobs.scottsdalebible.com/" width="100%;" height="100%;" frameborder="0" seamless="0" scrolling="auto"></iframe>
+
+
+<?php get_footer();
