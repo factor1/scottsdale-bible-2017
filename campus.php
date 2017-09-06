7082b177b52d2c51e3b140e8ecb3ed97e33e1c42
Dana <dana@factor1studios.com>
2017-09-06 11:38:26 -0700

    began structure for plan your visit section on home

--- campus.php
+++ campus.php
@@ -10,6 +10,7 @@ get_template_part("components/sections/upcoming-story");
 get_template_part("components/sections/last-and-upcoming-message");
 get_template_part("components/sections/upcoming-message");
 get_template_part("components/sections/news-and-stories");
+get_template_part("components/sections/plan-your-visit");
 get_template_part("components/sections/upcoming-events");
 get_template_part("components/sections/subscribe-to-news");
 get_template_part("components/sections/location-map");
---
+++ components/sections/plan-your-visit.php
@@ -0,0 +1,70 @@
+<?php if(!isset($wp)) { return; }
+
+if(!isset($post)) {
+    $post = get_queried_object();
+}
+$events_post_id = (get_field("use_homepage_events")) ? sb_get_homepage_post_id() : $post->ID;
+
+/* Load Preset Events if Set */
+$events = [];
+if($upcoming_events = get_field("upcoming_events",$events_post_id)) {
+    foreach($upcoming_events as $event) {
+        $event =& $event['event'];
+        $events[] = sb_load_all_event_data($event);
+    }
+}
+
+$events_link = ($f=get_field("events_category",$events_post_id)) ? get_term_link($f) : get_option('siteurl')."/events/";
+
+/* No Preset, Load Most Recent Posts */
+/* Inactive for Now
+if(!$events) {
+    $events = sb_get_upcoming_events();
+}
+*/
+
+if(!$events) {
+    return;
+}
+
+?>
+<section class="plan-your-visit">
+    <div class="row">
+      <div class="medium-2 columns"> &nbsp</div>
+      <div class="medium-8 columns">
+        <h1>Plan Your Visit</h1>
+      </div>
+      <div class="medium-2 columns"> &nbsp</div>
+    </div>
+    <div class="row">
+        <ul class="small-block-grid-1 large-block-grid-2">
+            <?php foreach($events as $event) { ?>
+            <li>
+                <div class="row">
+                    <?php if($image=get_field("featured_image",$event->ID)) { ?>
+                    <div class="medium-6 columns">
+                        <a href="<?php echo get_permalink($event->ID); ?>" class="image-block-center"><img src="<?php echo esc_attr($image['sizes']['event_home']); ?>" alt="" title="" /></a>
+                    </div>
+                    <?php } ?>
+                    <div class="medium-6 columns">
+                        <h3>
+                            <a href="<?php echo get_permalink($event->ID); ?>"><?php echo esc_html($event->post_title); ?></a>
+                        </h3>
+                        <?php if($field=get_field("event_subtitle",$event->ID)) { ?>
+                        <h5><?php echo esc_html($field); ?></h5>
+                        <?php } ?>
+                        <?php if($field=get_field("event_excerpt",$event->ID)) { ?>
+                        <div>
+                            <?php echo $field; ?>
+                        </div>
+                        <?php } ?>
+                    </div>
+                </div>
+            </li>
+            <?php } ?>
+        </ul>
+    </div>
+    <div class="row">
+        <a href="<?php echo esc_attr($events_link); ?>" class="button dark-brown">See <span>all</span> events</a>
+    </div>
+</section>
