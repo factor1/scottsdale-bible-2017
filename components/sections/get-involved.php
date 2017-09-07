cd1875798900031f532967a1bde10554d57f6f51
Dana <dana@factor1studios.com>
2017-09-07 15:57:00 -0700

    added file path for buttons in get involved section

--- components/sections/get-involved.php
+++ components/sections/get-involved.php
@@ -5,8 +5,8 @@
             <div>
                 <h1>Get Involved</h1>
                 <p>
-                    <a href="#" class="button-second" data-target="new-window">Classes & Groups</a>
-                    <a href="#" class="button-second" data-target="new-window">All Events</a>
+                    <a href="/smallgroups" class="button-second" data-target="new-window">Classes & Groups</a>
+                    <a href="/events" class="button-second" data-target="new-window">All Events</a>
                 </p>
             </div>
         </div>
