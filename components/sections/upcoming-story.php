66fcbf074663417846717dc7566f6e6e1302be59
Dana <dana@factor1studios.com>
2017-09-07 16:56:19 -0700

    working to make upcoming-story section dynamic

--- components/sections/upcoming-story.php
+++ components/sections/upcoming-story.php
@@ -1,6 +1,20 @@
-<?php if(!isset($wp)) { return; } ?>
+<?php if(!isset($wp)) { return; }
+
+if(!isset($post)) {
+    $post = get_queried_object();
+}
+$story_post_id = (get_field("use_homepage_story")) ? sb_get_homepage_post_id() : $post->ID;
+
+$upcoming_story = get_field("upcoming_story",$story_post_id);
+
+if(!$upcoming_story) {
+    return;
+}
+
+?>
 <section class="upcoming-story">
     <div class="row">
+        <?php if($upcoming_story) { ?>
         <div class="small-12 columns">
             <div>
                 <img src="<?php echo get_template_directory_uri() ?>/images/holders/LifeisbetterConnectedlogo.png" />
@@ -8,5 +22,6 @@
                 <a href="#" class="button" data-target="new-window">Learn More</a>
             </div>
         </div>
+        <?php } ?>
     </div>
 </section>
