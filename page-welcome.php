b1f398224ea33afd71bd0f41c8bfb8da170532f9
Dana <dana@factor1studios.com>
2017-09-11 14:51:08 -0700

    building new interior page  template 'Welcome Full'

---
+++ page-welcome.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * Template Name: Welcome Full
+ * Description: Page template with media rich content
+ */
+
+get_header();
+get_template_part("components/sections/featured-image");
+get_template_part("components/sections/welcome");
+get_template_part("components/sections/first-visit");
+get_template_part("components/sections/general-info");
+get_template_part("components/sections/page-content");
+get_footer();
