82cbbc007b88b29a79e8fd8c24204c5540b964b1
Dana <dana@factor1studios.com>
2017-09-11 14:15:49 -0700

    home hero creation (still needs video functionality)

--- campus.php
+++ campus.php
@@ -4,7 +4,8 @@ $campus = get_queried_object();
 sb_set_campus_cookie($campus->ID);
 
 get_header();
-get_template_part("components/sections/image-slider");
+
+get_template_part("components/sections/home-hero");
 get_template_part("components/sections/get-involved");
 get_template_part("components/sections/upcoming-story");
 get_template_part("components/sections/last-and-upcoming-message");
