16f9b8b3faa27480d5a82e44cc2c57e0b9cc910b
Dana <dana@factor1studios.com>
2017-09-12 15:00:27 -0700

    simplified flip js

--- js/global.js
+++ js/global.js
@@ -2,10 +2,5 @@ var $=jQuery.noConflict();
 
 $(document).ready(function(){
     // jQuery code is in here
-    $("#card1").flip();
-    $("#card2").flip();
-    $("#card3").flip();
-    $("#card4").flip();
-    $("#card5").flip();
-    $("#card6").flip();
+    $('#card1,#card2,#card3,#card4,#card5,#card6').flip();
 });
