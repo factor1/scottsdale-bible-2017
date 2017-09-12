35618da6d37d662dacad1e831890cd8887f8d333
Dana <dana@factor1studios.com>
2017-09-12 15:24:29 -0700

    added style and template files for Live Page Template

---
+++ components/sections/message-info.php
@@ -0,0 +1,22 @@
+<?php if(!isset($wp)) { return; } ?>
+
+<?php if( have_rows ('get_involved') ): ?>
+<section class="get-involved">
+    <div class="row">
+        <div class="small-12 columns">
+            <div>
+                <h1>Get Involved</h1>
+                <?php while ( have_rows('get_involved') ) : the_row();
+
+                // vars
+                $button = get_sub_field('button_text');
+                $button_link = get_sub_field('button_link');
+
+                ?>
+                <a href="<?php echo $button_link ?>" class="button-second button-involved" data-target="new-window"><?php echo $button ?></a>
+                <?php endwhile; ?>
+            </div>
+        </div>
+    </div>
+</section>
+<?php endif;  ?>
---
+++ page-live.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * Template Name: Live
+ * Description: Page template for live stream
+ */
+
+get_header();
+get_template_part("components/sections/live-message");
+get_template_part("components/sections/message-info");
+
+get_footer();
