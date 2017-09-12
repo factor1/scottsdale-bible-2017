4d13331e1bb66b252b9488bd4bfb5e46c649b7ff
Dana <dana@factor1studios.com>
2017-09-12 10:27:43 -0700

    updated ACF fields and scss of general information section

--- components/sections/general-info.php
+++ components/sections/general-info.php
@@ -1,28 +1,28 @@
 <?php if(!isset($wp)) { return; } ?>
-<?php if( have_rows ('plan_your_visit', 5) ): ?>
+<?php if( have_rows ('general_info') ) : the_row() ?>
 <section class="general-info">
     <div class="row">
         <ul class="small-block-grid-1 medium-block-grid-2 large-block-grid-3">
-            <?php while ( have_rows('plan_your_visit', 5) ) : the_row();
+            <?php if( have_rows ('info_blocks') ): ?>
+              <?php while ( have_rows('info_blocks') ) : the_row();
 
-            // vars
-            $image = get_sub_field('image');
-            $venue = get_sub_field('name');
-            $button = get_sub_field('button_text');
-            $button_link = get_sub_field('button_link');
+              // vars
+              $image = get_sub_field('image');
+              $title = get_sub_field('title');
+              $text_area = get_sub_field('text');
 
-            ?>
-            <li>
-                <div class="row">
-                    <div class="medium-12 columns box-section" style="background-image: url(<?php echo esc_attr($image['sizes']['large']); ?>);">
-                        <div class="box-section-text">
-                            <h2><?php echo $venue ?></h2>
-                            <a href="<?php echo $button_link ?>" class="button" data-target="new-window"><?php echo $button ?></a>
-                        </div>
-                    </div>
-                </div>
-            </li>
-            <?php endwhile; ?>
+              ?>
+              <li>
+                  <div class="row">
+                      <div class="medium-12 columns box-section" style="background-image: url(<?php echo esc_attr($image['sizes']['large']); ?>);">
+                          <div class="box-section-text">
+                              <h2><?php echo $title ?></h2>
+                          </div>
+                      </div>
+                  </div>
+              </li>
+              <?php endwhile; ?>
+            <?php endif;  ?>
         </ul>
     </div>
 </section>
