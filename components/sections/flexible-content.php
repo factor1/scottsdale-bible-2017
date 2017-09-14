7eba5de4a1fd08e7fa2e3dfc7bb90882bf9ce165
Dana <dana@factor1studios.com>
2017-09-14 13:26:32 -0700

    unfinished compilation of flex content field and partials

--- components/sections/flexible-content.php
+++ components/sections/flexible-content.php
@@ -1,35 +1,38 @@
 <?php if(!isset($wp)) { return; } ?>
 
-<?php if( have_rows ('flexible-content') ): ?>
+<?php if( have_rows('flexible_content', 198) ): ?>
 <section class="flexible-content">
 
 <?php
 
 		// loop through all the rows of flexible content
-		while ( have_rows('home_page_content') ) : the_row();
+		while ( have_rows('flexible_content', 198) ) : the_row();
 
 		// Callout 1
-		if( get_row_layout() == 'callout_1' )
-			get_template_part('partials/stripe', 'callout-1');
+		if( get_row_layout() == 'callout_1' ) {
+			get_template_part('partials/stripe', 'callout1');
+    };
 
 		// Callout 2
-		if( get_row_layout() == 'callout_2' )
-			get_template_part('partials/stripe', 'callout-2');
-
+		if( get_row_layout() == 'callout_2' ) {
+			get_template_part('partials/stripe', 'callout2');
+    };
 		// Callout 3
-		if( get_row_layout() == 'callout_3' )
-			get_template_part('partials/stripe', 'callout-3');
-
+		if( get_row_layout() == 'callout_3' ) {
+			get_template_part('partials/stripe', 'callout3');
+    };
 		// Callout 4
-		if( get_row_layout() == 'callout_4' )
-			get_template_part('partials/stripe', 'callout-4');
-
+		if( get_row_layout() == 'callout_4' ) {
+			get_template_part('partials/stripe', 'callout4');
+    };
 		// General Info
-		if( get_row_layout() == 'general_info' )
+		if( get_row_layout() == 'general_info' ) {
 			get_template_part('partials/stripe', 'general-info');
-
+    };
 		endwhile; // close the loop of flexible content
-?>
 
-</section>
+else : // no layouts found
+  echo "Not Found";
+?>
 <?php endif; // close flexible content conditional ?>
+</section>
