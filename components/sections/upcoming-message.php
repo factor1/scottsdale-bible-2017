7f9788e640656018e12b38750e50c858f141762c
Dana <dana@factor1studios.com>
2017-10-04 14:12:41 -0700

    last week and upcoming message sections now conditional

--- components/sections/upcoming-message.php
+++ components/sections/upcoming-message.php
@@ -5,15 +5,15 @@ if(!isset($post)) {
 }
 $msg_post_id = (get_field("use_homepage_messages")) ? sb_get_homepage_post_id() : $post->ID;
 
-$last_weekend = ($f=get_field("last_weekend_message",$msg_post_id)) ? (object) $f[0] : null;
 $upcoming_message = ($f=get_field("upcoming_message",$msg_post_id)) ? (object) $f[0] : null;
 
-if(!$last_weekend||!$upcoming_message) {
+if(!$upcoming_message) {
     return;
 }
 
 ?>
 
+<?php if($upcoming_message) { ?>
 <section class="upcoming-message" style="background-image: url(<?php echo esc_attr($upcoming_message->background_image['sizes']['large']); ?>);">
     <div>
         <?php if($upcoming_message->subtitle && $upcoming_message->subline_option === true) { ?>
@@ -24,3 +24,4 @@ if(!$last_weekend||!$upcoming_message) {
         <?php } ?>
     </div>
 </section>
+<?php } ?>
